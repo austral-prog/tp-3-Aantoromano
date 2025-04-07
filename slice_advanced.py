5867698c7010ca6b8cea9612e3ad16003312208c
Aantoromano <aromano3@mail.austral.edu.ar>
2025-04-07 15:51:35 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -5,6 +5,5 @@ def slice_advanced():
     #Hello, World!
     print(texto[4: :2]) 
 
-
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
